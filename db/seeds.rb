165f65ccff755b7f55c9869a33012268b21ed396
Naohiro Takahashi <31355405+zyuunananoharukanatu@users.noreply.github.com>
2018-03-11 17:00:06 +0900

    ちょっと修正
    なんかコメントアウトしてたので、復活させました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,20 @@
 require "csv"
 
 # job_ms
-#CSV.foreach('db/sample/job_ms.csv') do |row|
-#    JobM.create(:id => row[0] ,:job_name => row[1], :is_deleted => row[2])
-#end
+CSV.foreach('db/sample/job_ms.csv') do |row|
+    JobM.create(:id => row[0] ,:job_name => row[1], :is_deleted => row[2])
+end
 
 # position_ms
-#CSV.foreach('db/sample/position_ms.csv') do |row|
-#    PositionM.create(:id => row[0] ,:position_name => row[1], :is_deleted => row[2])
-#end
+CSV.foreach('db/sample/position_ms.csv') do |row|
+    PositionM.create(:id => row[0] ,:position_name => row[1], :is_deleted => row[2])
+end
 
 # users_infos
-#CSV.foreach('db/sample/users_infos.csv') do |row|
-#    UsersInfo.create(:id => row[0] ,:password => row[1], :user_name => row[2], :job_m_id => row[3], :position_m_id  => row[4],
-#    :age => row[5], :is_deleted => row[6], :address => row[7], :version => row[8])
-#end
+CSV.foreach('db/sample/users_infos.csv') do |row|
+    UsersInfo.create(:id => row[0] ,:password => row[1], :user_name => row[2], :job_m_id => row[3], :position_m_id  => row[4],
+    :age => row[5], :is_deleted => row[6], :address => row[7], :version => row[8])
+end
 
 # companies_infos
 CSV.foreach('db/sample/companies_infos.csv') do |row|
