2ee3369adb626f9255cdfa73fbeaae399257077a
froomeee <zyuunananoharukanatu@gmail.com>
2018-03-11 07:35:49 +0000

    会社情報と評価のseedを追加　あとマイグレートも修正

--- db/migrate/20180307153012_create_job_ms.rb
+++ db/migrate/20180307153012_create_job_ms.rb
@@ -1,8 +1,8 @@
 class CreateJobMs < ActiveRecord::Migration[5.0]
   def change
     create_table :job_ms do |t|
-      t.string :job_name
-      t.integer :is_deleted
+      t.string :job_name, :limit => 50, null: false
+      t.integer :is_deleted, :limit => 1, null: false, :default => 0
 
       t.timestamps
     end
--- db/migrate/20180307153105_create_users_infos.rb
+++ db/migrate/20180307153105_create_users_infos.rb
@@ -1,14 +1,14 @@
 class CreateUsersInfos < ActiveRecord::Migration[5.0]
   def change
     create_table :users_infos do |t|
-      t.string :password
-      t.string :user_name
+      t.string :password, :limit => 60, null: false
+      t.string :user_name, :limit => 20, null: false
       t.references :job_m, foreign_key: true
       t.references :position_m, foreign_key: true
-      t.integer :age
-      t.integer :is_deleted
-      t.text :address
-      t.string :version
+      t.integer :age, :limit => 1, null: false
+      t.integer :is_deleted, :limit => 1, null: false, :default => 0
+      t.text :address, :limit => 320, null: false
+      t.string :version, :limit => 5
 
       t.timestamps
     end
