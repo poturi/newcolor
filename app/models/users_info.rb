2b2c3272e508ee87816a131f72b995977bbb580f
froomeee <zyuunananoharukanatu@gmail.com>
2018-03-18 02:51:42 +0000

    ユーザ登録実装　バリデーションはまだ

--- app/models/users_info.rb
+++ app/models/users_info.rb
@@ -1,4 +1,6 @@
 class UsersInfo < ApplicationRecord
   belongs_to :job_m
   belongs_to :position_m
+  
+  AGES = [[NOT_SET, 0], [TEENS, 1], [TWENTIES, 2], [THIRTIES, 3], [FORTIES, 4], [FIFTIES, 5], [SIXTIES, 6]]
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,12 @@
+## 定数定義
+module Constants
+  
+  ## 年代
+  NOT_SET = '設定しない'
+  TEENS = '10代'
+  TWENTIES = '20代'
+  THIRTIES = '30代'
+  FORTIES = '40代'
+  FIFTIES = '50代'
+  SIXTIES = '60代'
+end
