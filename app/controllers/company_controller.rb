0071adbb96e387a9f340462874057225bec36260
tomoya <tomozou4606@yahoo.co.jp>
2018-06-16 02:25:44 +0000

    詳細ってどんななのか

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,5 +1,16 @@
 class CompanyController < ApplicationController
   def info
+    
+        
     @companyInfo = CompaniesInfo.find(params[:id])
+    
+    @jobInfo = JobM.find(@companyInfo.job_m_id)
+    
+    @ratingInfo = Rating.where("companies_info_id = ?", params[:id])
+    
+    if @companyInfo.url.nil? then
+      @companyInfo.url = ""
+    end
+
   end
 end
